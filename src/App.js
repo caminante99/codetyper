8cc763f669d92fb81a3dd654a06674a76e5806c0
Soutrik <valusoutrik@gmail.com>
2021-04-01 21:29:55 +0530

    Initial commit and api key loading from .env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+};
+
+console.log(firebaseConfig)
+
+
+
 function App() {
   return (
     <div className="App">
